88b7cbb0ec0cb3031ff18e885dc3e39b405d1d37
JulioLascano <jflascano@gmail.com>
2021-07-14 22:31:01 -0300

    Con cambios en app.js para adaptarlo a Heroku

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.listen(3030, () => console.log('Servidor corriendo en el puerto 3030'));
+// app.listen(3030, () => console.log('Servidor corriendo en el puerto 3030'));
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Servidor corriendo en el puerto 3030');
+    })
 
 
 app.get('/', (req, res) => {
